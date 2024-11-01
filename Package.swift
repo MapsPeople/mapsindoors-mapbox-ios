83cddcf70eaf9dbb7afbbf267b645490da278dd8
Christian Wolf Johannsen <chwo@mapspeople.com>
2024-11-01 13:34:07 +0100

    Only top FW dynamic

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapsindoorsVersion = Version("4.6.3-beta.3")
+let mapsindoorsVersion = Version("4.6.3-beta.4")
 
 let package = Package(
     name: "MapsIndoorsMapbox",
