ef36aacb40916263889fec44cda6cdc7b51e6369
Christian Wolf Johannsen <chwo@mapspeople.com>
2025-07-22 10:55:12 +0200

    Release version 4.12.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapsindoorsVersion = Version("4.12.2")
+let mapsindoorsVersion = Version("4.12.3")
 
 let package = Package(
     name: "MapsIndoorsMapbox",
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/MapsPeople/mapsindoors-core-ios.git", exact: mapsindoorsVersion),
-        .package(url: "https://github.com/mapbox/mapbox-maps-ios.git", exact: "11.13.0"),
+        .package(url: "https://github.com/mapbox/mapbox-maps-ios.git", exact: "11.13.3"),
     ],
     targets: [
         .target(
