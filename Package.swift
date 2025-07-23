b6e82985777f804741632b3398e060eb45131d1d
Christian Wolf Johannsen <chwo@mapspeople.com>
2025-07-23 14:57:12 +0200

    Release version 4.13.0-rc.1

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapsindoorsVersion = Version("4.12.3")
+let mapsindoorsVersion = Version("4.13.0-rc.1")
 
 let package = Package(
     name: "MapsIndoorsMapbox",
--- Sources/MapsIndoorsMapbox/MBProjectionModel.swift
+++ Sources/MapsIndoorsMapbox/MBProjectionModel.swift
@@ -23,22 +23,33 @@ class MBProjectionModel: MPProjection {
             let nearRight = view.mapboxMap.coordinate(for: CGPoint(x: view.frame.width, y: view.frame.height))
             let center = view.mapboxMap.coordinate(for: CGPoint(x: view.frame.width / 2, y: view.frame.height / 2))
 
-            // Add a 100% buffer to the viewport area
+            
+            // Determine the buffer factor based on the current zoom level
+            // This is a heuristic to improve the percieved performance of the map by adding a buffer to the viewport area,
+            // pre-rendering areas outside the viewport. A user is less likely to notice "pop-in" while panning/zooming.
+            // Obviously this is costly, the larger the buffer becomes - so we gradually increase the buffer size based on the zoom level.
+            let zoom = view.mapboxMap.cameraState.zoom
+            let bufferFactor = switch zoom {
+                case 0..<16: 1.0 // Add no buffer to the viewport area
+                case 16..<20: 2.0 // Add a 100% buffer to the viewport area
+                case 20..<25: 3.0 // Add a 200% buffer to the viewport area
+                default: 1.0
+            }
 
             let nearLeftBearingToCenter = MPGeometryUtils.bearingBetweenPoints(from: nearLeft, to: center) + 180
-            let nearLeftDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: nearLeft), to: MPGeoPoint(coordinate: center)) * 2
+            let nearLeftDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: nearLeft), to: MPGeoPoint(coordinate: center)) * bufferFactor
             let nearLeftOffset = MPGeometryUtils.computeOffset(from: nearLeft, dist: nearLeftDistanceToCenter, head: nearLeftBearingToCenter)
 
             let nearRightBearingToCenter = MPGeometryUtils.bearingBetweenPoints(from: nearRight, to: center) + 180
-            let nearRighDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: nearRight), to: MPGeoPoint(coordinate: center)) * 2
+            let nearRighDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: nearRight), to: MPGeoPoint(coordinate: center)) * bufferFactor
             let nearRighOffset = MPGeometryUtils.computeOffset(from: nearRight, dist: nearRighDistanceToCenter, head: nearRightBearingToCenter)
 
             let farLeftBearingToCenter = MPGeometryUtils.bearingBetweenPoints(from: farLeft, to: center) + 180
-            let farLeftDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: farLeft), to: MPGeoPoint(coordinate: center)) * 2
+            let farLeftDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: farLeft), to: MPGeoPoint(coordinate: center)) * bufferFactor
             let farLeftOffset = MPGeometryUtils.computeOffset(from: farLeft, dist: farLeftDistanceToCenter, head: farLeftBearingToCenter)
 
             let farRightBearingToCenter = MPGeometryUtils.bearingBetweenPoints(from: farRight, to: center) + 180
-            let farRightDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: farRight), to: MPGeoPoint(coordinate: center)) * 2
+            let farRightDistanceToCenter = MPGeometryUtils.distance(from: MPGeoPoint(coordinate: farRight), to: MPGeoPoint(coordinate: center)) * bufferFactor
             let farRightOffset = MPGeometryUtils.computeOffset(from: farRight, dist: farRightDistanceToCenter, head: farRightBearingToCenter)
 
             let bufferedRegion = MPGeoRegion(nearLeft: nearLeftOffset,
