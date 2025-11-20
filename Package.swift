276e85a579092fafb45c8757ca43f10a82631be7
Christian Wolf Johannsen <chwo@mapspeople.com>
2025-11-20 14:48:47 +0100

    Release version 4.16.0-rc.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapsindoorsVersion = Version("4.16.0-rc.2")
+let mapsindoorsVersion = Version("4.16.0-rc.3")
 
 let package = Package(
     name: "MapsIndoorsMapbox",
